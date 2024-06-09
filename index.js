a3cc819200f86a1c5c6989e8503d074e75eea594
Rahi <125520697+RahiMisra@users.noreply.github.com>
2024-06-09 00:19:28 -0700

    Added links and projects.

--- index.js
+++ index.js
@@ -50,3 +50,65 @@ function toggleTheme(theme) {
         document.body.classList.add('theme-black');
     }
 }
+
+const projects = {
+    myHealth: {
+        image: "icons/java.svg",
+        description: "Android application designed to assist dialysis patients with managing their disease and lifestyle.",
+        link: "#"
+    },
+    hotel: {
+        image: "icons/java.svg",
+        description: "Application for scheduling appointments at a hotel.",
+        link: "#"
+    },
+    qaAuto: {
+        image: "icons/python.svg",
+        description: "Automation script that populates a database with csv reports and can export specific reports based on searches.",
+        link: "#"
+    },
+    fmAuto: {
+        image: "icons/python.svg",
+        description: "Automation script that reads a list of frames to be edited, finds their corresponding locations, exports a report, and uploads specific frame ranges to online collaboration platform Frame.io.",
+        link: "#"
+    },
+    fileSort: {
+        image: "icons/python.svg",
+        description: "Automatic file sorter utilizing Artificial Intelligence to analyze files and sort them.",
+        link: "#"
+    },
+    tennis: {
+        image: "icons/python.svg",
+        description: "Implemented machine learning algorithms on a dataset of tennis match results and measured predictive capabilities.",
+        link: "https://github.com/sharkyx101/542Project"
+    },
+    flight: {
+        image: "icons/python.svg",
+        description: "Utilized data mining techniques on a dataset of flight costs to find underlying relationships.",
+        link: "#"
+    },
+    trafficLight: {
+        image: "icons/c.svg",
+        description: "Traffic light simulation on an ESP32 board utilizing interrupts for timing changes.",
+        link: "#"
+    },
+    potentiometer: {
+        image: "icons/c.svg",
+        description: "Reading a potentiometer and reflecting its position with a servo and led display on an ESP32 boad.",
+        link: "#"
+    }
+};
+
+function toggleProject(projectId) {
+    const project = projects[projectId];
+    if (project) {
+        const imageElement = document.getElementById("project_icon");
+        imageElement.src = project.image;
+
+        const descriptionElement = document.getElementById("project_description_text");
+        descriptionElement.textContent = project.description;
+
+        const linkElement = document.getElementById("project_link_url");
+        linkElement.href = project.link;
+    }
+}
